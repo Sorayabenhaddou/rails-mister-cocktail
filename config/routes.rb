619c69aa85643c49bfccf16a722a6db61b5bf0d8
Sorayabenhaddou <soraya-b55@hotmail.fr>
2020-05-14 16:56:35 +0200

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+   # get "cocktails", to: "cocktails#index"
+   # get "cocktails/new", to: "cocktails#new"
+   # get "cocktails/:id", to: "cocktails#show", as: :cocktail
+   # post "cocktail", to: "cocktail#create"
+   # get "cocktails/:id/edit", to: "cocktails#edit"
+
+   # get "doses", to: "doses#index"
+   # get "doses/new", to: "doses#new"
+   # get "doses/:id", to: "doses#show", as: :review
+   # post "dose", to: "dose#create"
+   # get "doses/:id/edit", to: "doses#edit"
+   # patch "dose/:id", to: "dose#update"
+
+  resources :cocktails do
+    resources :doses, only: [:new, :create, :update]
+  end
+   resources :doses, only: [:destroy]
 end
