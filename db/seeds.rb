6c82ebc756a41d1967574f7181c0176675a952a7
Sorayabenhaddou <soraya-b55@hotmail.fr>
2020-05-15 15:05:42 +0200

    exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-
-Cocktail.create(name: "Cuba Libre")
+# COCKTAILS
+puts "Cleaning database..."
+Cocktail.destroy_all
+puts "Creating cocktails..."
+martini = { name: "Martini" }
+pina_collada = { name: "Pina collada" }
+negroni = { name: "Negroni" }
+paloma = { name: "Paloma" }
+[ martini, pina_collada, negroni, paloma ].each do |attributes|
+  cocktail = Cocktail.create!(attributes)
+  puts "Created #{cocktail.name}"
+end
+puts "Finished!"
+# INGREDIENTS
+puts "Cleaning database..."
+Ingredient.destroy_all
+puts "Creating ingredients..."
+lemon = { name: "lemon" }
+ice = { name: "ice" }
+mint = { name: "mint leaves" }
+[ lemon, ice, mint ].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
+puts "Finished!"
